c8aadd88d701ee43a57eab14e3c8dc51b1404b90
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-15 00:10:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 const driver = {}
 function updateDriverWithKeyAndValue(driver, key, value){
-  return driver.name = 'Sam'
+  driver.name = 'Sam'
   driver.address = '11 Broadway'
 }
