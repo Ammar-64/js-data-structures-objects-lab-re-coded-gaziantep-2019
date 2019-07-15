959f8d67a46afcaa5932c6a3d07f9c3d1d1a7b8b
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-15 00:17:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your solution in this file!
 const driver = {}
 
-function updateDriverWithKeyAndValue(obj, key ,value){
-  return Object.assign({},obj,{[key]: value});
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({},driver,{[key] : value});
 }
 
 function updateDriverWithKeyAndValue(obj, key, value){
-  obj[key] = value;
-  return obj;
+  driver.name = 'sam';
+  driver.address = '12 Broadway';
 }
